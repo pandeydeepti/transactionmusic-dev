640d27bd19ef3e90a8f540aa083f128155bc21cf
pandeydeepti <35036316+pandeydeepti@users.noreply.github.com>
2018-01-12 14:22:52 +0530

    Nav Update

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -3,6 +3,8 @@
         <ul>
             <a href="{{url('admin/beats')}}"><li class="@if(strpos(Request::path(), 'admin/beats')!== false ) active @endif">BEATS</li></a>
             <a href="{{url('admin/categories')}}"><li class="@if(strpos(Request::path(), 'admin/categories')!== false ) active @endif">CATEGORIES</li></a>
+            <a href="{{url('admin/sounds_like')}}"><li class="@if(strpos(Request::path(), 'admin/sounds_like')!== false ) active @endif">SOUNDS LIKE</li></a>
+            <a href="{{url('admin/producers')}}"><li class="@if(strpos(Request::path(), 'admin/producers')!== false ) active @endif">PRODUCERS</li></a>
             <a href="{{url('admin/pages')}}"><li class="@if(strpos(Request::path(), 'admin/pages')!== false )active @endif">PAGES</li></a>
             <a href="{{url('admin/embed')}}"><li class="@if(Request::path() == 'admin/embed') active @endif">EMBED</li></a>
             <a href="{{url('admin/banners')}}"><li class="@if(Request::path() == 'admin/banners') active @endif">BANNERS</li></a>
