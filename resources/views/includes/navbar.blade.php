92f08814b86eeabc841047621930aeaaf8f7f43b
pandeydeepti <35036316+pandeydeepti@users.noreply.github.com>
2018-02-06 12:46:42 +0530

    Update navbar.blade.php

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -7,7 +7,6 @@
     <div class="navbar-links main-nav-header-wrapper">
         <button class="menu-toggle main-color"><i class="fa fa-bars" aria-hidden="true"></i></button>
         <ul class="clearfix main-navigation">
-            <li><a href="{{url('/')}}">HOME</a></li>
             @if(!empty($pages))
                 @foreach($pages as $page)
                     <li><a href="{{url('/'.$page->slug)}}"><li>{{$page->title}}</a></li>
@@ -16,7 +15,6 @@
             @if((strpos($faqs, '1') !== false))
                 <li><a href="{{url('/faq')}}">FAQ</a></li>
             @endif
-            <li><a href="{{url('/contact')}}">CONTACT US</a></li>
         </ul>
     </div>
 </div>
